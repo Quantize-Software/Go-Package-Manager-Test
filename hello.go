8d32e85de670c022a06a323c740ee4cf0cf5c56e
mikezamora <contact@mike-zamora.com>
2023-10-11 22:59:45 -0600

    added node exec

--- hello.go
+++ hello.go
@@ -1,7 +1,16 @@
-package main
+package init
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+)
 
-func main() {
+func init() {
 	fmt.Println("Hello World from go main function in github package!")
+	data, err := exec.Command("node", "data/server.js").Output()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(data))
 }
