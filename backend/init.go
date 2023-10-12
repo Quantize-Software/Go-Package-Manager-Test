7596dae8a52c8726af06906c20e537067885fc8d
mikezamora <contact@mike-zamora.com>
2023-10-12 00:16:45 -0600

    wd

--- backend/init.go
+++ backend/init.go
@@ -2,12 +2,16 @@ package backend
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 )
 
 func Init() {
 	fmt.Println("Hello World from go main function in github package!")
-	data, err := exec.Command("node", "data/server.js").Output()
+	path, _ := os.Getwd()
+	fmt.Println("Working Dir:", path)
+
+	data, err := exec.Command("node", path+"/data/server.js").Output()
 	if err != nil {
 		panic(err)
 	}
